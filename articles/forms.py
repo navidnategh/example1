d61defabab9516e2a68078bb1dda17d9fcbe331b
navidnategh <nateghnavid@gmail.com>
2020-08-08 13:28:39 +0430

    update creat forms

---
+++ articles/forms.py
@@ -0,0 +1,9 @@
+
+from django import forms
+
+from . import models
+
+class CreatArticle (forms.ModelForm) :
+    class Meta :
+        model = models.Article
+        fields = ['title' , 'slug' , 'body' , 'image' ]
--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from . import models
 from django.shortcuts import HttpResponse
-
 from django.contrib.auth.decorators import login_required
-
+from django.shortcuts import redirect
+from . import forms
 # Create your views here.
 
 def articles_list(request) :
@@ -13,15 +13,22 @@ def articles_list(request) :
 
     return render(request, 'articles/articles_list.html',arg)
 
-
 def article_detail (request , slug ) :
      # return HttpResponse (slug)
      article = models.Article.objects.get(slug=slug)
 
      return render (request , 'articles/article_detail.html',{'article':article})
 
-
-
 @login_required(login_url='/accounts/login')
+
 def creat_article(request) :
-    return render (request, 'articles/creat_article.html')
+    if request.method == 'POST' :
+        form = forms.CreatArticle(request.POST , request.FILES)
+        if form.is_valid:
+            # save
+            return redirect('articles:list')
+
+    else:
+        form = forms.CreatArticle()
+
+    return render (request, 'articles/creat_article.html', {  'form' : form  })
